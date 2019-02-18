40ca99c709394dddde0dcb33eebef6dff9a474d5
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2019-02-18 21:53:47 +0300

    init commit

---
+++ src/main/java/ru/mityushin/responder/ResponderApplication.java
@@ -0,0 +1,13 @@
+package ru.mityushin.responder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponderApplication.class, args);
+    }
+
+}
