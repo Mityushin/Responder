03ba8b9218a95ae93c8d4eac5f47edd58e837439
Dmitry Mityushin <mityushin.dmitry@gmail.com>
2019-02-22 15:28:04 +0300

    rework exception handler

--- src/main/java/ru/mityushin/responder/controller/CallbackResponseEntityExceptionHandler.java
+++ src/main/java/ru/mityushin/responder/controller/CallbackResponseEntityExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import ru.mityushin.responder.util.exception.MessageSenderException;
 
 /**
  * Exception handler for requests
@@ -18,16 +17,8 @@ import ru.mityushin.responder.util.exception.MessageSenderException;
 public class CallbackResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     private static final String DEFAULT_RESPONSE = "ok";
 
-    @ExceptionHandler({MessageSenderException.class})
+    @ExceptionHandler({RuntimeException.class})
     protected ResponseEntity<Object> handleOk(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex, DEFAULT_RESPONSE, null, HttpStatus.OK, request);
-    }
-
-    @ExceptionHandler({
-            UnsupportedOperationException.class,
-            NullPointerException.class
-    })
-    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(), null, HttpStatus.BAD_REQUEST, request);
     }
 }
